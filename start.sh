a0bc95c62ad3d15991efaa76420887b1318c15d4
N. Escobar <nick@nickesc.io>
2026-02-09 13:56:41 -0500

    fixed start script ordering

--- start.sh
+++ start.sh
@@ -4,9 +4,11 @@ pip3 install -r requirements.txt
 
 npm install --prefix app
 
-# start frontend
-npm run dev --prefix app
-
 # start backend
 mkdir -p logs
 uvicorn server.main:app --port 8127 --reload >logs/backend.log 2>&1 &
+
+# start frontend
+npm run dev --prefix app
+
+
