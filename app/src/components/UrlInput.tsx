9c0dc767d2698f7776eb4265c73884629115f727
N. Escobar <nick@nickesc.io>
2026-02-09 14:33:21 -0500

    wiring up basic url submit

--- app/src/components/UrlInput.tsx
+++ app/src/components/UrlInput.tsx
@@ -1,8 +1,24 @@
-function UrlInput() {
+import type {SubmitEvent} from "react";
+import {useState} from "react";
+
+function UrlInput({onAnalyze}: {onAnalyze: (url: string) => Promise<void>}) {
+    const [url, setUrl] = useState("");
+
+    function handleSubmit(event: SubmitEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log(url);
+        onAnalyze(url);
+    }
+
     return (
-        <form id="url-input-form">
-            <input type="text" placeholder="Enter article URL" />
-            <button>Analyze</button>
+        <form id="url-input-form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Enter article URL"
+                value={url}
+                onChange={(event) => setUrl(event.target.value)}
+            />
+            <button type="submit">Analyze</button>
         </form>
     );
 }
